28834327c1d9faebf605862f62311113f9483c59
Samet.Tastan <netflix.uyelik84@gmail.com>
2021-12-09 22:58:24 +0300

    js first lesson

---
+++ js-101/script.js
@@ -0,0 +1,34 @@
+console.log("Merhaba script")
+
+//alert("Safyada Uyarı Verir")
+// window.alert("Safyada Uyarı ")
+document.write("<p> Merhaba</p>")
+
+
+let firstName="Mehmet" 
+let lastName="Türk"
+const age= 46;
+let birthdate="1965"
+
+console.log(firstName, lastName, birthdate, "dogumlu", age)
+
+let dataString= Merhaba
+
+// console.log("Mehmet 46 yaşında")
+//console.log(firstname, lastname, age, "yaşında")
+// console.log("Memet 46 yaşında")
+
+// let name
+//PascalCase
+// let FirstName="Mehmet"; 
+
+//camelcase  //Tavsiye edilen
+
+// let firstName="Mehmet";
+
+//Snakecase
+ // let first_name="Mehmet";
+
+//Kebap Case
+
+// let firstname="Mehmet" //Css de kullanılıyor.
